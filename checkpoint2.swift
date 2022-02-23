8574cd90a6b75a23fb06c90de2c26354193b5bc2
Junior <juniorcastro2314@gmail.com>
2022-02-23 17:42:37 -0300

    removing some files

--- checkpoint2.swift
+++
@@ -1,5 +0,0 @@
-let numbers = [1, 1, 2, 2, 3, 4, 4, 5, 6, 7, 8, 8, 8, 9]
-print(numbers.count)
-
-let unique = Set(numbers)
-print(unique.count)
--- sets.swift
+++
@@ -1,8 +0,0 @@
-let actors = Set(["Denzel Washington", "Tom Cruise", "Nicolas Cage", "Samuel L Jackson"])
-print(actors)
-
-var actors2 = Set<String>()
-actors2.insert("Tom Holland")
-actors2.insert("Zendaya")
-actors2.insert("Hunter Schafer")
-print(actors2) 
