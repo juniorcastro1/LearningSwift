54367be0849f5c1fc5ac01b714385fa4b3c3592b
Junior <juniorcastro2314@gmail.com>
2022-02-28 16:34:06 -0300

    Adding lesson about switch statements

---
+++ Day5/switchStatements.swift
@@ -0,0 +1,45 @@
+switch forecast {
+case .sun:
+    print("It should be a nice day.")
+case .rain: 
+    print("Pack an Umbrella.")
+case .wind:
+     print("Wear something warm.")
+case .snow:
+    print("School is cancelled.")
+case .unknown:
+    print("Our forecast generator is broken!")
+}
+
+let place = "Metropolis"
+
+switch place {
+case "Gotham":
+    print("You're Batman!")
+case "Mega-City One":
+    print("You're Judge Dredd!")
+case "Wakanda":
+    print("You're Black Panther")
+default:
+    print("Who are you?")
+}
+
+let day = 5
+print("My true love gave to me…")
+
+switch day {
+case 5:
+    print("5 golden rings")
+    fallthrough
+case 4:
+    print("4 calling birds")
+    fallthrough
+case 3:
+    print("3 French hens")
+    fallthrough
+case 2:
+    print("2 turtle doves")
+    fallthrough
+default:
+    print("A partridge in a pear tree")
+}
