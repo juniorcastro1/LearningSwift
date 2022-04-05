732cc1cddedea78b26b317b1f25dff11c05a8578
Junior <juniorcastro2314@gmail.com>
2022-04-05 14:23:20 -0300

    class initializers lesson

---
+++ Day12/classInitializers.swift
@@ -0,0 +1,16 @@
+class Vehicle {
+    let isElectric: Bool
+
+    init(isElectric: Bool) {
+        self.isElectric = isElectric
+    }
+}
+
+class Car: Vehicle {
+    let isConvertible: Bool
+
+    init(isElectric: Bool, isConvertible: Bool) {
+        self.isConvertible = isConvertible
+        super.init(isElectric)
+    }
+}
