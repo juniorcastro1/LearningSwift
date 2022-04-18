53c704e3c6cd220d2ab26b2d345c7b777052714e
Junior <juniorcastro2314@gmail.com>
2022-04-17 23:57:54 -0300

    function failure with optionals lesson

---
+++ Day14/5functionFailureOptionals.swift
@@ -0,0 +1,14 @@
+enum UserError: Error {
+    case badID, networkFailed
+}
+
+func getUser(id: Int) throws -> String {
+    throw UserError.networkFailed
+}
+
+if let user = try? getUser(id: 23) {
+    print("User: \(user)")
+}
+
+let user = (try? getUser(id: 23)) ?? "Anonymous"
+print(user)
